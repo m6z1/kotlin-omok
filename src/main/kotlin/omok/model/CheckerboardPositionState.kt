1111581a2f2d1432b0c07797c5b176db5e26be29
m6z1 <sonmyungj1zz@gmail.com>
2025-03-18 20:39:36 +0900

    feat: CheckerboardPositionState 구현

--- src/main/kotlin/omok/model/CheckerboardPositionState.kt
+++ src/main/kotlin/omok/model/CheckerboardPositionState.kt
@@ -4,4 +4,13 @@ enum class CheckerboardPositionState {
     EMPTY,
     EXIST_BLACK_STONE,
     EXIST_WHITE_STONE,
+    ;
+
+    fun put(stone: Stone): CheckerboardPositionState {
+        check(this == EMPTY) { "이미 돌이 있습니다." }
+        return when (stone) {
+            Stone.BLACK -> EXIST_BLACK_STONE
+            Stone.WHITE -> EXIST_WHITE_STONE
+        }
+    }
 }
