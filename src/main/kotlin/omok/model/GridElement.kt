5546af43f9730eac2d642f5ca658a9b6124fc10b
m6z1 <sonmyungj1zz@gmail.com>
2025-03-19 09:47:42 +0900

    refactor: GridElement 검증 조건 수정
    As-is) 0~14 사이의 값만 허용
    To-be) 0 이상의 값만 허용

--- src/main/kotlin/omok/model/GridElement.kt
+++ src/main/kotlin/omok/model/GridElement.kt
@@ -9,11 +9,10 @@ value class DefaultGridElement(
     override val value: Int,
 ) : GridElement {
     init {
-        require(value in MIN_VALUE..MAX_VALUE)
+        require(value >= MIN_VALUE) { "그리드 요소는 ${MIN_VALUE}이상의 값만 가능합니다." }
     }
 
     companion object {
         private const val MIN_VALUE = 0
-        private const val MAX_VALUE = 14
     }
 }
