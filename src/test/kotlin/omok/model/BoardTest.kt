c27e4a396444e5d2fc9729918526dfda57c8d9d9
m6z1 <sonmyungj1zz@gmail.com>
2025-03-20 16:45:54 +0900

    refactor: 인터페이스에 의존하도록 테스트 변수 타입 수정

--- src/test/kotlin/omok/model/BoardTest.kt
+++ src/test/kotlin/omok/model/BoardTest.kt
@@ -1,9 +1,12 @@
 package omok.model
 
+import omok.model.board.Board
+import omok.model.board.BoardPosition
 import omok.model.board.BoardPositionState
 import omok.model.board.DefaultBoard
 import omok.model.board.DefaultBoardPosition
 import omok.model.position.DefaultPosition
+import omok.model.position.Position
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -12,7 +15,7 @@ class BoardTest {
     fun `원하는 한 변의 길이를 받아 정사각형의 보드판을 만들 수 있다`() {
         // given:
         // when:
-        val board = DefaultBoard(10)
+        val board: Board = DefaultBoard(10)
 
         // then:
         assertThat(board.positions.size).isEqualTo(100)
@@ -21,8 +24,8 @@ class BoardTest {
     @Test
     fun `특정 위치에 있는 오목판의 상태를 알 수 있다`() {
         // given:
-        val position = DefaultPosition(1, 2)
-        val board = DefaultBoard(DefaultBoardPosition(position, BoardPositionState.Exist.White))
+        val position: Position = DefaultPosition(1, 2)
+        val board: Board = DefaultBoard(DefaultBoardPosition(position, BoardPositionState.Exist.White))
 
         // when:
         val actual: BoardPositionState = board.stateOf(position)
@@ -34,9 +37,9 @@ class BoardTest {
     @Test
     fun `원하는 위치에 돌을 둘 수 있다`() {
         // given:
-        val position = DefaultPosition(1, 2)
-        val boardPosition = DefaultBoardPosition(position)
-        val board = DefaultBoard(boardPosition)
+        val position: Position = DefaultPosition(1, 2)
+        val boardPosition: BoardPosition = DefaultBoardPosition(position)
+        val board: Board = DefaultBoard(boardPosition)
 
         // when:
         board.put(position, Stone.WHITE)
