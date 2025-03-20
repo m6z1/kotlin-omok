47e8e7b840a88bf7af5c130f99d505c0fcc72d36
m6z1 <sonmyungj1zz@gmail.com>
2025-03-20 16:14:02 +0900

    refactor: controller 내부 함수 분리

--- src/main/kotlin/omok/controller/Omok.kt
+++ src/main/kotlin/omok/controller/Omok.kt
@@ -1,5 +1,6 @@
 package omok.controller
 
+import omok.model.board.Board
 import omok.model.board.DefaultBoard
 import omok.model.gameState.BlackTurn
 import omok.model.gameState.GameState
@@ -10,34 +11,46 @@ class Omok(
     private val omokView: OmokView,
 ) {
     fun play() {
-        val board = DefaultBoard()
-        omokView.start(board)
+        val board = Board()
         var currentTurn: GameState = BlackTurn
-        var lastTurn: GameState.Playing
         var lastPosition: Position? = null
-        var position: Position =
-            omokView.position(
-                stone = currentTurn.stone,
-                boundary = board.sideLength.value,
-            )
+        var position: Position = position(currentTurn, board)
         while (currentTurn is GameState.Playing) {
-            lastTurn = currentTurn
+            val lastTurn: GameState.Playing = currentTurn
             currentTurn = currentTurn.play(board, position)
-            if (!currentTurn.forbidden(lastTurn)) {
-                lastPosition = position
-            }
+            lastPosition = lastPosition(currentTurn, lastTurn, lastPosition, position)
             omokView.show(board)
             if (currentTurn is GameState.Finish) break
-            position =
-                omokView.position(
-                    stone = currentTurn.stone,
-                    boundary = board.sideLength.value,
-                    lastPosition = lastPosition,
-                    forbiddenPosition = if (currentTurn.forbidden(lastTurn)) lastPosition else null,
-                )
+            position = position(currentTurn, board, lastPosition, lastTurn)
         }
         omokView.show(currentTurn.stone)
     }
 
-    private fun GameState.forbidden(lastTurn: GameState.Playing): Boolean = this == lastTurn
+    private fun Board(): Board {
+        val board = DefaultBoard()
+        omokView.start(board)
+        return board
+    }
+
+    private fun position(
+        currentTurn: GameState,
+        board: Board,
+        lastPosition: Position? = null,
+        lastTurn: GameState.Playing? = null,
+    ): Position =
+        omokView.position(
+            stone = currentTurn.stone,
+            boundary = board.sideLength.value,
+            lastPosition = lastPosition,
+            forbiddenPosition = if (currentTurn.forbidden(lastTurn)) lastPosition else null,
+        )
+
+    private fun lastPosition(
+        currentTurn: GameState,
+        lastTurn: GameState.Playing,
+        lastPosition: Position?,
+        position: Position,
+    ): Position? = if (currentTurn.forbidden(lastTurn)) lastPosition else position
+
+    private fun GameState.forbidden(lastTurn: GameState.Playing?): Boolean = this == lastTurn
 }
