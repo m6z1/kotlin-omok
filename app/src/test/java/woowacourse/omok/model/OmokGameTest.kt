5e93570eb729ac048491f79b14504dcd5e57873d
m6z1 <sonmyungj1zz@gmail.com>
2025-03-29 13:08:37 +0900

    test: 변경된 도메인 테스트 수정

--- app/src/test/java/woowacourse/omok/model/OmokGameTest.kt
+++ app/src/test/java/woowacourse/omok/model/OmokGameTest.kt
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import woowacourse.omok.model.board.Board
 import woowacourse.omok.model.gameState.BlackTurn
-import woowacourse.omok.model.gameState.GameState
-import woowacourse.omok.model.gameState.WhiteTurn
+import woowacourse.omok.model.gameState.PutState
 import woowacourse.omok.model.position.Position
+import woowacourse.omok.model.testDouble.BLACK_EXIST_STONE_IF_PUT_1_3
 import woowacourse.omok.model.testDouble.BLACK_WIN_BOARD_IF_PUT_1_5
 import woowacourse.omok.model.testDouble.POSITION_1_5
 
@@ -27,59 +27,55 @@ class OmokGameTest {
     }
 
     @Test
-    fun `흑돌턴 때 받은 포지션이 오목을 만드는 경우 흑돌의 Finish를 반환한다`() {
+    fun `흑돌턴 때 받은 포지션이 돌을 놓을 수 있는 경우 CanPut 을 반환한다`() {
         val board: Board = BLACK_WIN_BOARD_IF_PUT_1_5
         val omokGame = OmokGame(board)
         val position: Position = POSITION_1_5
 
-        val actual: GameState = omokGame.getTurnState(position)
+        val actual: PutState = omokGame.checkPutState(position)
 
-        assertThat(actual).isEqualTo(GameState.Finish(Stone.BLACK))
+        assertThat(actual).isEqualTo(PutState.CanPutStone)
     }
 
     @Test
-    fun `백돌턴 때 받은 포지션이 오목을 만드는 경우 백돌의 Finish를 반환한다`() {
-        omokGame.getTurnState(Position(1, 1)) // 흑돌 턴 1
-        omokGame.getTurnState(Position(8, 1)) // 백돌 턴 1
-        omokGame.getTurnState(Position(1, 3)) // 흑돌 턴 2
-        omokGame.getTurnState(Position(8, 2)) // 백돌 턴 2
-        omokGame.getTurnState(Position(1, 4)) // 흑돌 턴 3
-        omokGame.getTurnState(Position(8, 3)) // 백돌 턴 3
-        omokGame.getTurnState(Position(1, 5)) // 흑돌 턴 4
-        omokGame.getTurnState(Position(8, 4)) // 백돌 턴 4
-        omokGame.getTurnState(Position(1, 8)) // 흑돌 턴 5
-        val actual: GameState = omokGame.getTurnState(Position(8, 5)) // 백돌 턴 5
+    fun `백돌턴 때 받은 포지션이 돌을 놓을 수 있는 경우 CanPut 을 반환한다`() {
+        omokGame.checkPutState(Position(1, 1)) // 흑돌 턴 1
+        omokGame.checkPutState(Position(8, 1)) // 백돌 턴 1
+        omokGame.checkPutState(Position(1, 3)) // 흑돌 턴 2
+        omokGame.checkPutState(Position(8, 2)) // 백돌 턴 2
+        omokGame.checkPutState(Position(1, 4)) // 흑돌 턴 3
+        omokGame.checkPutState(Position(8, 3)) // 백돌 턴 3
+        omokGame.checkPutState(Position(1, 5)) // 흑돌 턴 4
+        omokGame.checkPutState(Position(8, 4)) // 백돌 턴 4
+        omokGame.checkPutState(Position(1, 8)) // 흑돌 턴 5
+        val actual: PutState = omokGame.checkPutState(Position(8, 5)) // 백돌 턴 5
 
-        assertThat(actual).isEqualTo(GameState.Finish(Stone.WHITE))
+        assertThat(actual).isEqualTo(PutState.CanPutStone)
     }
 
     @Test
-    fun `흑돌턴 때 받은 포지션이 금수일 경우 같은 흑돌턴을 반환한다`() {
-        omokGame.getTurnState(Position(1, 3)) // 흑돌 턴 1
-        omokGame.getTurnState(Position(8, 1)) // 백돌 턴 1
-        omokGame.getTurnState(Position(2, 3)) // 흑돌 턴 2
-        omokGame.getTurnState(Position(8, 2)) // 백돌 턴 2
-        omokGame.getTurnState(Position(3, 2)) // 흑돌 턴 3
-        omokGame.getTurnState(Position(8, 3)) // 백돌 턴 3
-        omokGame.getTurnState(Position(3, 4)) // 흑돌 턴 4
-        omokGame.getTurnState(Position(8, 4)) // 백돌 턴 4
-        val actual = omokGame.getTurnState(Position(3, 3)) // 흑돌 턴 5
+    fun `흑돌턴 때 받은 포지션이 금수일 경우 같은 ForbiddenStone 을 반환한다`() {
+        omokGame.checkPutState(Position(1, 3)) // 흑돌 턴 1
+        omokGame.checkPutState(Position(8, 1)) // 백돌 턴 1
+        omokGame.checkPutState(Position(2, 3)) // 흑돌 턴 2
+        omokGame.checkPutState(Position(8, 2)) // 백돌 턴 2
+        omokGame.checkPutState(Position(3, 2)) // 흑돌 턴 3
+        omokGame.checkPutState(Position(8, 3)) // 백돌 턴 3
+        omokGame.checkPutState(Position(3, 4)) // 흑돌 턴 4
+        omokGame.checkPutState(Position(8, 4)) // 백돌 턴 4
+        val actual = omokGame.checkPutState(Position(3, 3)) // 흑돌 턴 5
 
-        assertThat(actual).isEqualTo(BlackTurn)
+        assertThat(actual).isEqualTo(PutState.ForbiddenStone)
     }
 
     @Test
-    fun `흑돌턴 때 받은 포지션이 흑돌을 놓을 수 있으면 흑돌턴을 반환한다`() {
-        val actual: GameState = omokGame.getTurnState(Position(1, 3))
+    fun `흑돌턴 때 받은 포지션이 이미 돌이 놓여있으면 ExistStone 을 반환한다`() {
+        val board = BLACK_EXIST_STONE_IF_PUT_1_3
+        val omokGame = OmokGame(board)
+        val position = Position(1, 3)
 
-        assertThat(actual).isEqualTo(WhiteTurn)
-    }
+        val actual: PutState = omokGame.checkPutState(position)
 
-    @Test
-    fun `백돌턴 때 받은 포지션이 백돌을 놓을 수 있으면 흑돌턴을 반환한다`() {
-        omokGame.getTurnState(Position(8, 1)) // 흑돌턴
-        val actual: GameState = omokGame.getTurnState(Position(8, 2)) // 백돌턴
-
-        assertThat(actual).isEqualTo(BlackTurn)
+        assertThat(actual).isEqualTo(PutState.ExistStone)
     }
 }
--- app/src/test/java/woowacourse/omok/model/testDouble/Fixture.kt
+++ app/src/test/java/woowacourse/omok/model/testDouble/Fixture.kt
@@ -30,6 +30,12 @@ val BLACK_FORBIDDEN_BOARD_IF_PUT_1_1 =
         put(3, 1, Stone.BLACK)
     }
 
+val BLACK_EXIST_STONE_IF_PUT_1_3 =
+    Board().apply {
+        put(1, 2, Stone.BLACK)
+        put(1, 3, Stone.BLACK)
+    }
+
 val POSITION_1_5 = Position(1, 5)
 
 val POSITION_1_0 = Position(1, 0)
